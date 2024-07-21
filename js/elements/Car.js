b547be27fe7373d21f1ff582ce4bba82631740cb
Ashish kumar Verma <imdigitalashish@gmail.com>
2024-07-21 13:20:43 +0530

    Added Event Listeners to Our to Cute Car

---
+++ js/elements/Car.js
@@ -0,0 +1,21 @@
+import { Controls } from "../utils/controls.js";
+
+export class Car {
+    constructor(x, y, width, height) {
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.controls = new Controls();
+
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.rect(this.x- this.width/ 2, this.y - this.height / 2, this.width, this.height);
+
+        ctx.fill();
+    }
+}
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+import { Car } from "./js/elements/Car.js";
+
 class Application {
     constructor() {
         this.canvas = document.querySelector("canvas");
         this.ctx = this.canvas.getContext('2d');
 
-        this.canvas.width = 600;
-        this.canvas.height = 600;
+        this.canvas.width = 200;
+        this.canvas.height = window.innerHeight;
 
         this.ctx = this.canvas.getContext('2d');
+
+        this.car = new Car(100, 100, 50, 50);
+        this.car.draw(this.ctx);
         
     }
 }
